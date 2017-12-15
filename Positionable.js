ea7afa1127d1b8041615ec5e3a21c0d4aa0f08bc
Lis, Scott <Scott_Lis@Intuit.com>
2017-12-15 16:29:00 -0700

    init

---
+++ Positionable.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+
+class Positionable extends Component {
+  render() {
+    return (
+      <div style={props.positionStyle}>
+        PositionMe
+      </div>
+    );
+  }
+}
+
+export default App;
