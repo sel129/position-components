553cebcdb61cea09c37e1bfc1b3d5e615f65bd77
Lis, Scott <Scott_Lis@Intuit.com>
2017-12-19 16:47:02 -0700

    experimenting

--- src/App.js
+++ src/App.js
@@ -2,11 +2,28 @@ import React, { Component } from 'react';
 import './App.css';
 
 class App extends Component {
+  position(originalComponent) {
+    const style1={
+      position: 'absolute',
+      'backgroundColor': 'red',
+      top: '10px'
+    }
+    const style2={
+      position: 'absolute',
+      'backgroundColor': 'blue',
+      top: '50px'
+    }
+    return originalComponent(style1);
+  }
+
   render() {
+    const child = (position) => {
+      return (<div style={position}>component1</div>);
+    }
+    const div1 = this.position(child)
     return (
       <div>
-        <div>component 1</div>
-        <div>component 2</div>
+        {div1}
       </div>
     );
   }
